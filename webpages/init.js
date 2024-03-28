dbbf9db89b4c9783bd4b3604a81c48d4a92dc4f8
surasak_po <surasak.po1413@gmail.com>
2024-03-28 16:16:31 +0700

    INIT WEB

---
+++ webpages/init.js
@@ -0,0 +1,65 @@
+function showLoading() {
+    loadingSwal = Swal.fire({
+        title: 'Loading...',
+        allowOutsideClick: false,
+        allowEscapeKey: false,
+        showConfirmButton: false,
+        willOpen: () => {
+            Swal.showLoading();
+        }
+    });
+}
+
+function closeLoading() {
+    if (loadingSwal) {
+        loadingSwal.close();
+    }
+}
+
+
+function showMessage(type = "success", message) {
+
+    Swal.fire({
+        title: message,
+        icon: type || "success"
+    });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    window.accessToken = "";
+    window.Profile = null;
+    const liffId = "2004358435-rDXj330L";
+    function runApp() {
+        window.accessToken = liff.getAccessToken();
+        liff.getProfile().then(profile => {
+            window.Profile = {
+                picture_url: profile.pictureUrl || '',
+                display_name: profile.displayName || '',
+                status_message: profile.statusMessage || '',
+            }
+        });
+    }
+
+    try {
+        showLoading();
+        liff.init({
+            liffId: liffId,
+        }).then(() => {
+            if (liff.isLoggedIn()) {
+                runApp()
+            } else {
+                liff.login();
+            }
+        }).catch((err) => {
+            liff.login();
+        });
+
+    } catch (err) {
+
+    } finally {
+        closeLoading();
+    }
+
+
+
+});
